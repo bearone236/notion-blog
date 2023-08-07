772a8e7ea9a02af43b6ab6a55a3d8b6e8e6feec9
bearone236 <kuma_2306@icloud.com>
2023-08-08 02:24:57 +0900

    create notion-blog

---
+++ lib/notionAPI.ts
@@ -0,0 +1,66 @@
+import { Client } from '@notionhq/client';
+import { NotionToMarkdown } from 'notion-to-md';
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+export const getAllPosts = async () => {
+  const posts = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    page_size: 100,
+  });
+
+  const allPosts = posts.results;
+
+  return allPosts.map((post) => {
+    return getPageMetaData(post);
+  });
+};
+
+const getPageMetaData = (post) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+
+    return allTags;
+  };
+
+  return {
+    id: post.id,
+    title: post.properties.Name.title[0].plain_text,
+    description: post.properties.Description.rich_text[0].plain_text,
+    date: post.properties.Date.date.start,
+    slug: post.properties.Slug.rich_text[0].plain_text,
+    tags: getTags(post.properties.Tags.multi_select),
+  };
+};
+
+export const getSinglePost = async (slug) => {
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    filter: {
+      property: 'Slug',
+      formula: {
+        string: {
+          equals: slug,
+        },
+      },
+    },
+  });
+
+  const page = response.results[0];
+  const metadata = getPageMetaData(page);
+  // console.log(metadata)
+  const mdBlocks = await n2m.pageToMarkdown(page.id);
+  const mdString = n2m.toMarkdownString(mdBlocks);
+  console.log(mdString);
+
+  return {
+    metadata,
+    markdown: mdString,
+  };
+};
